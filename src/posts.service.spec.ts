c44770cb6dded496ae640aea9dc960561b0f1008
Ionela Lazer <ionela.lazer@yandex.ru>
2025-09-02 23:37:24 +0300

    Провайдеры. Задание 2

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -20,13 +20,63 @@ describe('PostsService', () => {
     });
 
     it('should return all posts if called without options', () => {
-      // реализуйте тест-кейс
+      const result = postsService.findMany();
+      expect(result).toEqual(
+        posts.map((p) => expect.objectContaining(p))
+      );
     });
 
     it('should return correct posts for skip and limit options', () => {
-      // реализуйте тест-кейс
+      const result = postsService.findMany({ skip: 1, limit: 2 });
+      expect(result).toEqual([
+        expect.objectContaining(posts[1]),
+        expect.objectContaining(posts[2]),
+      ]);
     });
 
-    // реализуйте недостающие тест-кейсы
+    it('returns first N posts when only limit provided', () => {
+      const result = postsService.findMany({ limit: 2 });
+      expect(result).toEqual([
+        expect.objectContaining(posts[0]),
+        expect.objectContaining(posts[1]),
+      ]);
+    });
+
+    it('skips N posts when only skip provided', () => {
+      const result = postsService.findMany({ skip: 2 });
+      expect(result).toEqual([
+        expect.objectContaining(posts[2]),
+        expect.objectContaining(posts[3]),
+      ]);
+    });
+
+    it('returns empty array if skip > posts count', () => {
+      const result = postsService.findMany({ skip: 10 });
+      expect(result).toEqual([]);
+    });
+
+    it('returns all posts if limit > posts count', () => {
+      const result = postsService.findMany({ limit: 10 });
+      expect(result).toEqual(
+        posts.map((p) => expect.objectContaining(p))
+      );
+    });
+
+    it('returns empty array if limit = 0', () => {
+      const result = postsService.findMany({ limit: 0 });
+      expect(result).toEqual([]);
+    });
+
+    it('returns all posts if skip = 0', () => {
+      const result = postsService.findMany({ skip: 0 });
+      expect(result).toEqual(
+        posts.map((p) => expect.objectContaining(p))
+      );
+    });
+
+    it('works correctly if both skip=0 and limit=0', () => {
+      const result = postsService.findMany({ skip: 0, limit: 0 });
+      expect(result).toEqual([]);
+    });
   });
 });
